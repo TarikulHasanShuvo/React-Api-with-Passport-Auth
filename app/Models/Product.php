e7fef1b40a7e14af3a2d710ba93cdd63f8bd30fb
shaheen2013 <coredeveloper.2013@gmail.com>
2021-10-04 21:05:26 +0600

    add activity log package

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory,LogsActivity;
 
     protected $fillable = ['name','price','size','description'];
+
+    public function getActivitylogOptions()
+    {
+        return LogOptions::defaults()
+            ->logOnlyDirty();
+    }
 }
